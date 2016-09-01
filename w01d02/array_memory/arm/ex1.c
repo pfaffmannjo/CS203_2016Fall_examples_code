5391bb89806fcf3aae9313df166001b99a6536b5
Pfaffmann JO <pfaffmannjo@github.com>
2016-09-01 08:27:20 -0400

    update arm

--- w01d02/array_memory/arm/ex1.c
+++ w01d02/array_memory/arm/ex1.c
@@ -6,19 +6,21 @@ int main(int argc, char *argv[]) {
   int    aInt[10];
   double aDouble[10];
 
-  for(int cnt = 0; cnt < 10; cnt++) {
-    printf("%p\n", &aChar[cnt], (aChar + cnt) );
+  int cnt;
+  
+  for(cnt = 0; cnt < 10; cnt++) {
+    printf("%p  %p\n", &aChar[cnt], (aChar + cnt) );
   }
 
   printf("\n");
       
-  for(int cnt = 0; cnt < 10; cnt++) {
-    printf("%p\n", &aInt[cnt], (aInt + cnt) );
+  for(cnt = 0; cnt < 10; cnt++) {
+    printf("%p  %p\n", &aInt[cnt], (aInt + cnt) );
   }
 
   printf("\n");
       
-  for(int cnt = 0; cnt < 10; cnt++) {
-    printf("%p\n", &aDouble[cnt], (aDouble + cnt) );
+  for(cnt = 0; cnt < 10; cnt++) {
+    printf("%p  %p\n", &aDouble[cnt], (aDouble + cnt) );
   }
 }
